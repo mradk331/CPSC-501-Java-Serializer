80207971ec34b586014a47df6f217e7c3cde9527
Michael Radke <frederichenry070@gmail.com>
2017-11-13 21:55:41 -0700

    first xml seems to be good

--- src/Serializer.java
+++ src/Serializer.java
@@ -2,17 +2,20 @@ import java.beans.*;
 import java.io.*;
 import java.lang.*;
 
-import org.jdom2.*;
+import org.jdom.*;
+import org.jdom.output.XMLOutputter;
 
-
+import java.lang.reflect.*;
 import java.util.*;
 public class Serializer {
 
 	
-	Document root;
-	Element element;
+
+	static Element element = new Element("Serializer");
+	static Document root = new Document(element);
 	int choice = 0;
-	public void main(String[] args)
+	int roothash = 0;
+	public void main(String[] args) throws IllegalArgumentException, IllegalAccessException, IOException
 	{
 		
 		
@@ -27,10 +30,30 @@ public class Serializer {
 		
 	}
 	
-	public org.jdom2.Document Serializer(Object obj) {
+	public org.jdom.Document Serializer(Object obj) throws IllegalArgumentException, IllegalAccessException, IOException {
 		
 		
-		System.out.println(obj.getClass().getName());
+		System.out.println(obj.getClass().getName() + " class Passed in to Serializer");
+		//do this first I think
+		
+		
+		
+		root.setRootElement(element);
+		
+		
+		
+		
+		Element element = new Element ("Object");
+		
+		String hash =Integer.toString(obj.hashCode());
+		
+		if(roothash == 0)
+		roothash = obj.hashCode();
+		
+	//name followed by hash	
+		element.setAttribute(new Attribute("Class",obj.getClass().getSimpleName()));
+		element.setAttribute(new Attribute("ID", hash));
+		
 		
 		switch(choice)
 		{
@@ -39,6 +62,40 @@ public class Serializer {
 		// "primitive":
 		case 1:
 			
+		Element field = null;
+		Field[] fields = obj.getClass().getDeclaredFields();	
+		
+		for(int i = 0; i < fields.length; i++) {
+			field = new Element("Field");
+			Attribute attribute = new Attribute("Name" , fields[i].getName());
+			
+			field.setAttribute(attribute);
+			field.setAttribute("DeclaingClass", fields[i].getDeclaringClass().getSimpleName());
+			
+			
+			
+			Element value = new Element("Value");
+			fields[i].get(obj);  //vlue	
+			//System.out.println(fields[i].get(obj) + "pleasze do somethine");
+			value.addContent(fields[i].get(obj).toString());		
+			field.addContent(value);	
+			element.addContent(field);
+			//1root.getRootElement().addContent(element);
+				}
+		
+		
+		
+		
+		
+		
+			//element.addContent(field);
+			//root.getRootElement().addContent(element);
+		root.getRootElement().addContent(element);
+			//System.out.println(root. " Is there anything here?") ;
+			XMLOutputter out= new XMLOutputter();
+			out.output(root, new FileWriter("src//test.xml"));
+
+			break;
 			
 			
 		// "Oref":
@@ -62,8 +119,8 @@ public class Serializer {
 		
 		}
 		
-		
-		return null;
+		return root;
+		//return null;
 		
 		
 	}
